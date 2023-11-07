d0f26a8b60c9dedf03a2e4ddeaead78d4a1a0d08
pagidipalaravi <ravi.pagidipala2@inter.ikea.com>
2023-11-07 20:33:42 +0530

    Initial commit

---
+++ src/test/java/com/aktrix/dms/DmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aktrix.dms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
