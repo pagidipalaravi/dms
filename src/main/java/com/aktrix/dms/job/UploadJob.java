833f4edd7faa930240aa2cad9dccee3d458803cf
netheshguptha1 <nethesh.guptha1@gmail.com>
2023-11-10 09:30:19 +0530

    FileStorageController
    FileStorageController

--- src/main/java/com/aktrix/dms/job/UploadJob.java
+++ src/main/java/com/aktrix/dms/job/UploadJob.java
@@ -9,4 +9,14 @@ public class UploadJob {
 	public void getFilesJob() {
 		System.out.println("file will be upload for every ten seconds");
 	}
+=======
+
+@Component
+public class UploadJob {
+	@Scheduled(cron="*/10 * * * * *")
+	public void run() {
+		System.out.println("this method will be execute every ten seconds");
+	}
+
+>>>>>>> Stashed changes
 }
