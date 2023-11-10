e9f7b7b1f31a984637f9325571fa2a09a47bf2f3
netheshguptha1 <nethesh.guptha1@gmail.com>
2023-11-10 09:24:42 +0530

    uploadjob
    uploadjob

--- src/main/java/com/aktrix/dms/job/UploadJob.java
+++ src/main/java/com/aktrix/dms/job/UploadJob.java
@@ -1,8 +1,11 @@
 package com.aktrix.dms.job;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+@Component
 public class UploadJob {
-	
+	@Scheduled(cron="*/10 * * * * *")
 	public void getFilesJob() {
-		//TODO Write your code here
+		System.out.println("file will be upload for every ten seconds");
 	}
 }
