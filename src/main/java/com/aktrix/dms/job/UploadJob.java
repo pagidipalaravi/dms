d8989f97ed8813c93224a278d2bba2922e250e01
netheshguptha1 <nethesh.guptha1@gmail.com>
2023-11-10 09:33:57 +0530

    FileStorageController

--- src/main/java/com/aktrix/dms/job/UploadJob.java
+++ src/main/java/com/aktrix/dms/job/UploadJob.java
@@ -2,21 +2,12 @@ package com.aktrix.dms.job;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-<<<<<<< Updated upstream
+
 @Component
 public class UploadJob {
 	@Scheduled(cron="*/10 * * * * *")
 	public void getFilesJob() {
 		System.out.println("file will be upload for every ten seconds");
 	}
-=======
 
-@Component
-public class UploadJob {
-	@Scheduled(cron="*/10 * * * * *")
-	public void run() {
-		System.out.println("this method will be execute every ten seconds");
-	}
-
->>>>>>> Stashed changes
 }
