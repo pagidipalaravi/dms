a6333bc3bba734da4e05d7d1544518b68e41a800
netheshguptha1 <nethesh.guptha1@gmail.com>
2023-11-16 12:20:04 +0530

    pom.xml file
    pom.xml file

--- src/main/java/com/aktrix/dms/DmsApplication.java
+++ src/main/java/com/aktrix/dms/DmsApplication.java
@@ -2,10 +2,13 @@ package com.aktrix.dms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+
 public class DmsApplication {
 
 	public static void main(String[] args) {
